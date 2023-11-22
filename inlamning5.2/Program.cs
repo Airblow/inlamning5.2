30f6a28ced174d9c4bb522e4a4a035c32d7e2d7f
Airblow <143172177+Airblow@users.noreply.github.com>
2023-11-22 10:31:32 +0100

    Add project files.

---
+++ inlamning5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace inlamning5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = new string[4];
+            numbers[1] = "1997";
+            numbers[2] = "72";
+
+            Console.WriteLine("Skriv in valfritt heltal");
+            numbers[3] = Console.ReadLine();
+
+            Console.WriteLine($"{numbers[1]}, {numbers[2]}, {numbers[3]}");
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
